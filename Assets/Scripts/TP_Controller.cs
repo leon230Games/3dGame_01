ba7ecca0b519a2c5eb74f1e16293728e01032e29
Leon <homik.lukasz@gmail.com>
2017-09-19 23:43:44 +0200

    Add TP controller and motor scripts

---
+++ Assets/Scripts/TP_Controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP_Controller : MonoBehaviour {
+
+    public static CharacterController characterController;
+
+    #region Singleton
+    public static TP_Controller instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("Instance of EquipmentManager already exists");
+            return;
+        }
+
+        instance = this;
+
+        characterController = GetComponent<CharacterController>();
+    }
+    #endregion
+
+	
+
+	void Update () {
+
+        if (Camera.main == null)
+            return;
+
+        GetLocomotionInput();
+
+        TP_Motor.instance.UpdateMotor();
+	}
+
+    void GetLocomotionInput()
+    {
+        var deadZone = 0.1f;
+
+        TP_Motor.instance.moveVector = Vector3.zero;
+
+        if (Input.GetAxis("Vertical") > deadZone || Input.GetAxis("Vertical") < -deadZone)
+        {
+            TP_Motor.instance.moveVector += new Vector3(0, 0, Input.GetAxis("Vertical"));
+        }
+
+        if (Input.GetAxis("Horizontal") > deadZone || Input.GetAxis("Horizontal") < -deadZone)
+        {
+            TP_Motor.instance.moveVector += new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/TP_Motor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP_Motor : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+    public Vector3 moveVector { get; set; }
+
+    #region Singleton
+    public static TP_Motor instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("Instance of EquipmentManager already exists");
+            return;
+        }
+
+        instance = this;
+
+    }
+    #endregion
+
+    public void UpdateMotor() {
+
+        SnapCharWithCam();
+        ProcessMotion();
+    }
+
+    void ProcessMotion() {
+        //transform moveVector to WorldSpace
+
+        moveVector = transform.TransformDirection(moveVector);
+
+        //Normalize vector if magnitude > 0
+        if (moveVector.magnitude > 1)
+        {
+            moveVector = Vector3.Normalize(moveVector);
+        }
+        //Multiply moveVector by moveSpeed and delta time
+        moveVector *= moveSpeed * Time.deltaTime;
+        // move character in worldSpace
+
+        TP_Controller.characterController.Move(moveVector);
+    }
+
+    void SnapCharWithCam() {
+
+        if (moveVector.x != 0 || moveVector.z != 0)
+        {
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x,
+                Camera.main.transform.eulerAngles.y,
+                transform.eulerAngles.z);
+        }
+    }
+}
